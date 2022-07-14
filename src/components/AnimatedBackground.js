a0bf0484d5ab606673b995f396ddbe2dc7edab44
Egor Slobozhanin <true-akk@yandex.ru>
2022-07-14 09:02:03 +0400

    adaptive,gh pages

--- src/components/AnimatedBackground.js
+++ src/components/AnimatedBackground.js
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
+import note_img from "../img/music_notes_PNG61.png"
 
 const AnimatedBackground = () => {
 
   useEffect(() => {
     animate()
-  }, []);
+  }, [window.innerWidth]);
 
   const animate = () => {
     let amount = 20;
@@ -18,13 +19,19 @@ const AnimatedBackground = () => {
       let duration = Math.random() * 5;
 
       let size = Math.random() * 0.04 * 100
+      
+
       drop.id = "image-drop";
-      drop.src = "/img/music_notes_PNG61.png"
+      drop.src = note_img
       drop.style.width = 1 + size + '%';
       drop.style.height = 'auto';
       drop.style.left = posX + 'px';
       drop.style.animationDelay = delay + 's';
       drop.style.animationDuration = 3 + duration + 's';
+      if (window.innerWidth <= 515) {
+        size = Math.random() * 0.08 * 100;
+        drop.style.width = 3 + size + '%';
+      }
       body.appendChild(drop);
       i++
     }
