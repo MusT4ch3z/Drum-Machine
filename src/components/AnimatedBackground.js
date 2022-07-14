ef1d6c5593800dd62d954392a04c56d7291a2f31
Egor Slobozhanin <true-akk@yandex.ru>
2022-07-14 20:13:15 +0400

    background changes

--- src/components/AnimatedBackground.js
+++ src/components/AnimatedBackground.js
@@ -13,13 +13,10 @@ const AnimatedBackground = () => {
     let i = 0;
     while (i < amount) {
       let drop = document.createElement('img');
-
       let posX = Math.floor(Math.random() * window.innerWidth)
       let delay = Math.random() * -30;
       let duration = Math.random() * 5;
-
       let size = Math.random() * 0.04 * 100
-      
 
       drop.id = "image-drop";
       drop.src = note_img
